70011e8d78774223055929e146802005fdc499e0
Taloengrat Poomchaiya <armaerkarm@gmail.com>
2021-08-27 18:06:00 +0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM openjdk:11
+
+USER root
+
+WORKDIR /usr/src/myapp
+
+COPY src/main/resources/static/taloengrat_cv.png /usr/src/myapp/
+
+EXPOSE 8080
+
+ADD target/cv_api-0.0.1-SNAPSHOT.jar cv_api-0.0.1-SNAPSHOT.jar
+
+ENTRYPOINT [ "java", "-jar", "cv_api-0.0.1-SNAPSHOT.jar" ]
+
---
+++ src/main/java/com/example/cv_api/controller/DownloadFileController.java
@@ -0,0 +1,42 @@
+package com.example.cv_api.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DownloadFileController {
+
+    @GetMapping("/download-cv")
+    public ResponseEntity<InputStreamResource> download() throws IOException {
+
+        File file = new ClassPathResource("/usr/src/myapp/taloengrat_cv.png").getFile();
+
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
+                .contentLength(file.length()).body(resource);
+    }
+
+    @GetMapping("/show-cv")
+    public ResponseEntity<byte[]> getImage() throws IOException {
+        File img = new File("/usr/src/myapp/taloengrat_cv.png");
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(Files.readAllBytes(img.toPath()));
+    }
+
+    @RequestMapping(value = "/")
+    public String requestMethodName() {
+        return "CV-API is Activated";
+    }
+}
